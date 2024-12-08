da427b308d439c95fadbf12276a9874b6360af74
Odysseus <a.andrusyk@steel.mil.ua>
2024-12-08 10:01:01 +0200

    Implement two modes in train.py module

--- training/train.py
+++ training/train.py
@@ -1,3 +1,4 @@
+import argparse
 import torch
 import torchvision
 import torchvision.transforms.v2 as transforms
@@ -7,25 +8,15 @@ from data.dataset import Dataset
 from models.centernet import ModelBuilder, input_height, input_width
 from training.encoder import CenternetEncoder
 
+parser = argparse.ArgumentParser()
+parser.add_argument('-o', '--overfit', action='store_true', help='overfit to 10 images')
+args = parser.parse_args()
+
+overfit = args.overfit
+
 dataset_val = torchvision.datasets.VOCDetection(
     root="../VOC", year="2007", image_set="val", download=False
 )
-dataset_val = torchvision.datasets.wrap_dataset_for_transforms_v2(dataset_val)
-
-trainingdata_indices = torch.tensor(
-    [955, 1025, 219, 66, 1344, 222, 865, 2317, 86, 1409]
-)
-
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-model = ModelBuilder(alpha=0.25).to(device)
-
-lr = 0.02
-parameters = list(model.parameters())
-optimizer = torch.optim.Adam(parameters, lr=lr)
-encoder = CenternetEncoder(input_height, input_width)
-
-EPOCHS = 5
-model.train(True)
 
 transform = transforms.Compose(
     [
@@ -34,17 +25,43 @@ transform = transforms.Compose(
         transforms.ToDtype(torch.float32, scale=True),
     ]
 )
+
+encoder = CenternetEncoder(input_height, input_width)
+
+dataset_val = torchvision.datasets.wrap_dataset_for_transforms_v2(dataset_val)
 torch_dataset = Dataset(dataset=dataset_val, transformation=transform, encoder=encoder)
 
+training_data = torch_dataset
+lr = 0.001
+desired_loss = 0.1
 
-training_data = torch.utils.data.Subset(torch_dataset, trainingdata_indices)
+if overfit:
+    # randomly chosen pictures for model training
+    # trainingdata_indices = torch.tensor([955, 1025, 219, 66, 1344, 222, 865, 2317, 86, 1409])
+
+    trainingdata_indices = torch.randperm(len(torch_dataset))[:10]
+    training_data = torch.utils.data.Subset(torch_dataset, trainingdata_indices)
+    lr = 0.01
+    desired_loss = 1.0
+
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+model = ModelBuilder(alpha=0.25).to(device)
+
+parameters = list(model.parameters())
+optimizer = torch.optim.Adam(parameters, lr=lr)
+
+model.train(True)
 
 batch_generator = torch.utils.data.DataLoader(
     training_data, num_workers=4, batch_size=12
 )
 
-for epoch in range(EPOCHS):
-    print("EPOCH {}:".format(epoch + 1))
+epoch = 1
+get_desired_loss = False
+
+while True:
+    print("EPOCH {}:".format(epoch))
     for _, data in enumerate(batch_generator):
         input_data, gt_data = data
         input_data = input_data.to(device).contiguous()
@@ -59,5 +76,13 @@ for epoch in range(EPOCHS):
         optimizer.step()
 
         print(loss_dict["loss"])
+        if loss_dict["loss"] < desired_loss:
+            get_desired_loss = True
+            break
+
+    if get_desired_loss:
+        break
+
+    epoch += 1
 
 torch.save(model.state_dict(), "../models/checkpoints/pretrained_weights.pt")
