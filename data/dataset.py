c926983fa82dcc22cb66a529338943cd6bac1933
Odysseus <a.andrusyk@steel.mil.ua>
2024-12-22 20:47:15 +0200

    Refactor predictions visualisation

--- data/dataset.py
+++ data/dataset.py
@@ -16,3 +16,16 @@ class Dataset(data.Dataset):
 
     def __len__(self):
         return len(self._dataset)
+
+
+class ImageDatasetWithLabels(data.Dataset):
+    def __init__(self, dataset, transformation):
+        self._dataset = dataset
+        self._transformation = transformation
+
+    def __getitem__(self, index):
+        img, _ = self._dataset[index]
+        return self._transformation(img)
+
+    def __len__(self):
+        return len(self._dataset)
