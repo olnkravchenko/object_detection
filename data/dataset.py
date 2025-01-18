679263f92b2e32c0235833d47c8cad946a59c6b1
Olena Kravchenko <alenakravchenko1@gmail.com>
2025-01-18 15:53:24 +0200

    rename argument

--- data/dataset.py
+++ data/dataset.py
@@ -3,13 +3,14 @@ from torch.utils import data
 
 
 class Dataset(data.Dataset):
-    def __init__(self, dataset, transformation, encoder):
-        self._dataset = dataset
+    def __init__(self, dataset_loader, transformation, encoder):
+        self._dataset_loader = dataset_loader
         self._transformation = transformation
         self._encoder = encoder
 
     def __getitem__(self, index):
-        img, lbl = self._dataset[index]
+        img, lbl = self._dataset_loader[index]
+        print(lbl)
         img_, bboxes_, labels_ = self._transformation(
             img, lbl.get("boxes", []), lbl.get("labels", [])
         )
@@ -17,4 +18,4 @@ class Dataset(data.Dataset):
         return img_, torch.from_numpy(lbl_encoded)
 
     def __len__(self):
-        return len(self._dataset)
+        return len(self._dataset_loader)
